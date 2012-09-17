4d2eaaaa83c1b2ccf456f2617254bab56c2699bc
hugo220390@gmail.com <hugo220390@gmail.com@b029c293-41de-5477-5c8d-c14714c3db6f>
2012-09-17 16:19:50 +0000

    Criação do projeto Maven.

---
+++ trunk/inwhichmovie/src/main/java/com/inwhichmovie/beans/Filme.java
@@ -0,0 +1,92 @@
+package com.inwhichmovie.beans;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "filme")
+public class Filme implements Comparable<Filme> {
+	@Column
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idFilme;
+	@Column
+	private String idImdb;
+	@Column
+	private String nome;
+	@Column
+	private String nomeOriginal;
+	@Column
+	private String ano;
+	private List<Musica> musicas;
+
+	@Override
+	public String toString() {
+		return "Filme [idFilme=" + idFilme + ", idImdb=" + idImdb + ", nome="
+				+ nome + ", nomeOriginal=" + nomeOriginal + ", ano=" + ano
+				+ ", musicas=" + musicas + "]";
+	}
+
+	public Integer getIdFilme() {
+		return idFilme;
+	}
+
+	public void setIdFilme(Integer idFilme) {
+		this.idFilme = idFilme;
+	}
+
+	public String getIdImdb() {
+		return idImdb;
+	}
+
+	public void setIdImdb(String idImdb) {
+		this.idImdb = idImdb;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeOriginal() {
+		return nomeOriginal;
+	}
+
+	public void setNomeOriginal(String nomeOriginal) {
+		this.nomeOriginal = nomeOriginal;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public List<Musica> getMusicas() {
+		return musicas;
+	}
+
+	public void setMusicas(List<Musica> musicas) {
+		this.musicas = musicas;
+	}
+
+	public int compareTo(Filme o) {
+		// if((nome.compareToIgnoreCase(o.getNome()))==0)
+		// return nomeOriginal.compareToIgnoreCase(o.getNomeOriginal());
+		if (nome == null)
+			nome = "";
+		return nome.compareToIgnoreCase(o.getNome());
+	}
+
+}
